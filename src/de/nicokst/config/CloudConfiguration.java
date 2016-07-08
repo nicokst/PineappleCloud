4e0bc6cc284c6cf1ef6673737784863775751dae
nicokst <keistniko@web.de>
2016-07-08 20:27:37 +0200

    final commit of day 1

--- src/de/nicokst/config/CloudConfiguration.java
+++ src/de/nicokst/config/CloudConfiguration.java
@@ -1,5 +1,30 @@
 package de.nicokst.config;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class CloudConfiguration {
+
+	private File f = new File("config.txt");
+
+	public void init() {
+		if (!f.exists()) {
+			try {
+				f.createNewFile();
+				PrintWriter pw = new PrintWriter(new FileWriter(f), true);
+				pw.println("sql-hostname=localhost");
+				pw.println("sql-username=username");
+				pw.println("sql-password=password");
+				pw.println("sql-database=database");
+				pw.println("port=4484");
+				pw.println("greeting=Hey %s!");
+				pw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }
--- src/de/nicokst/db/MySQL.java
+++ src/de/nicokst/db/MySQL.java
@@ -1,5 +1,42 @@
 package de.nicokst.db;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class MySQL {
+
+	private String hostname, username, password, database;
+
+	public MySQL(String hostname, String username, String password, String database) {
+		this.hostname = hostname;
+		this.username = username;
+		this.password = password;
+		this.database = database;
+	}
+
+	public String getDatabase() {
+		return database;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void connect() {
+		try {
+			DriverManager.getConnection("jdbc:mysql://" + hostname + ":3306/" + database, username, password);
+			System.out.println("[SQL] Connected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
